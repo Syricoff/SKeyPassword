aad55009734fc221b1f1fd33f50ae8bc64d659f6
Syricov <syricov50@gmail.com>
2022-10-27 21:15:29 +0300

    Base ui

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    pass
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication
+from PyQt5.QtWidgets import QMainWindow
+
+
+class Main(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("ui/MainWindow.ui", self)
+
+    def run(self):
+        pass
 
 
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = Main()
+    ex.show()
+    sys.exit(app.exec())
