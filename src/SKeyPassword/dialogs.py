de0e0c422dbc0a5a9ed1a09f1b069250eba58525
Syricov <syricov50@gmail.com>
2022-11-05 05:12:13 +0300

    Изменена логика поиска Доработано окно добавления пароля

--- src/SKeyPassword/dialogs.py
+++ src/SKeyPassword/dialogs.py
@@ -1,7 +1,7 @@
 from ui.ui_Add_password import Ui_Add_password
 from ui.ui_AboutApp import Ui_AboutApp
-from errors import (PasswordError, LoginError,
-                    AppError, CategoryError)
+from errors import (PasswordError, LoginError, AppError,
+                    CategoryError, DataExistError)
 
 from PyQt5.QtWidgets import QDialog, QPushButton, QDialogButtonBox
 import sqlite3
@@ -20,9 +20,19 @@ class AddPassword(QDialog, Ui_Add_password):
         self.con = sqlite3.connect("res/passwords.sqlite")
         self.loadAppsAndTypes(main_window)
         # Подключения кнопок
-        self.buttonBox.accepted.connect(self.accept)
-        self.buttonBox.rejected.connect(self.reject)
         self.save_button.clicked.connect(self.save)
+
+    def getItems(self) -> tuple[str, str, str, str]:
+        return tuple(map(str.strip,
+                         (self.category.currentText(),
+                          self.app.currentText(),
+                          self.login.text(),
+                          self.password.text())))
+
+    def loadAppsAndTypes(self, main):
+        """Подгружает и выводит списки приложений и категорий"""
+        self.app.addItems(main.loadApps())
+        self.category.addItems(main.loadTypes())
 
     def add_category_if(self):
         """Проверяет есть ли категория в базе данных
@@ -40,57 +50,54 @@ class AddPassword(QDialog, Ui_Add_password):
             self.con.commit()
 
     def validator(self):
-        category = self.category.currentText()
-        app = self.app.currentText()
-        login = self.login.text()
-        password = self.password.text()
-        if not app.strip():
-            raise AppError("Приложение")
-        if not login.strip():
-            raise LoginError("Логин")
-        if not category.strip():
-            raise CategoryError("Категория")
-        if not password.strip():
-            raise PasswordError("Пароль")
+        category, app, login, password = self.getItems()
+        if not app:
+            raise AppError("Поле Приложение не может быть пустым")
+        if not login:
+            raise LoginError("Поле Логин не может быть пустым")
+        if not category:
+            raise CategoryError("Поле Категория не может быть пустым")
+        if not password:
+            raise PasswordError("Поле Пароль не может быть пустым")
 
-    def add_to_db(self):
+    def is_entry_in_db(self):
+        cur = self.con.cursor()
+        result = cur.execute("""
+                            SELECT id
+                            FROM Passwords
+                            WHERE  app_type =
+                            (SELECT id FROM Types
+                            WHERE type_name = ?)
+                            AND app_name = ?
+                            AND login = ?
+                            """, self.getItems()[:3])
+        if result.fetchone() is not None:
+            raise DataExistError("Запись уже существует")
+
+    def add_to_db(self) -> bool | None:
         """Добавляет полученные данные в базу данных"""
         try:
             self.validator()  # Проверка на пустые поля
             self.add_category_if()  # Проверка наличия категории
+            self.is_entry_in_db()
             cur = self.con.cursor()
             cur.execute("""
                         INSERT INTO
                         Passwords(app_name, login, password, app_type)
-                        VALUES(?, ?, ?,
-                        (SELECT id FROM Types WHERE type_name = ?))
-                        """, (self.app.currentText(), self.login.text(),
-                        self.password.text(), self.category.currentText()))
+                        VALUES((SELECT id FROM Types WHERE
+                        type_name = ?),
+                        ?, ?, ?)
+                        """, self.getItems())
             self.con.commit()
             self.errors.hide()
+            return True
         except ValueError as e:
             self.errors.show()
-            self.errors.setText(f"Поле {e} не может быть пустым")
-            self.save_button.setEnabled(True)
-            self.password.setReadOnly(True)
-
-    def loadAppsAndTypes(self, main):
-        """Подгружает и выводит списки приложений и категорий"""
-        self.app.addItems(main.loadApps())
-        self.category.addItems(main.loadTypes())
-
-    def accept(self) -> None:
-        """Кнопка 'ок' """
-        return super().accept()
-
-    def reject(self) -> None:
-        """Кнопка 'отмена' """
-        return super().reject()
+            self.errors.setText(f"{e}")
 
     def save(self) -> None:
-        self.save_button.setDisabled(True)
-        self.password.setReadOnly(True)
-        self.add_to_db()
+        if self.add_to_db():
+            return self.accept()
 
 
 class AboutProgram(QDialog, Ui_AboutApp):
@@ -99,5 +106,5 @@ class AboutProgram(QDialog, Ui_AboutApp):
         self.setupUi(self)
         self.buttonBox.rejected.connect(self.reject)
 
-    def reject(self):
+    def reject(self) -> None:
         return super().reject()
